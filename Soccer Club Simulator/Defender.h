767dfc5e4cc9fc2c33a7eda056cc174acdd954b3
EvanStowell <150971025+EvanStowell@users.noreply.github.com>
2025-03-30 12:30:46 -0400

    Removed keywords

--- Soccer Club Simulator/Defender.h
+++ Soccer Club Simulator/Defender.h
@@ -2,8 +2,8 @@
 // Created by Evan on 2/22/2025.
 //
 
-#ifndef M2OEP_ELSTOWEL_DEFENDER_H
-#define M2OEP_ELSTOWEL_DEFENDER_H
+#ifndef SOCCER_CLUB_SIMULATOR_DEFENDER_H
+#define SOCCER_CLUB_SIMULATOR_DEFENDER_H
 
 #include "Player.h"
 
@@ -42,4 +42,4 @@ public:
 };
 
 
-#endif //M2OEP_ELSTOWEL_DEFENDER_H
+#endif //SOCCER_CLUB_SIMULATOR_DEFENDER_H
